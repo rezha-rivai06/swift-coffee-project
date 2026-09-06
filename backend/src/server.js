de5572a714c672fbd9e8c028a631360a49c723c7
rezha-rivai06 <rezharivai06@gmail.com>
2026-09-07 00:16:31 +0700

    fix ui

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,8 @@ const reservasiRoutes = require('./modules/reservasi/reservasi.routes');
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Database Connection Middleware
 app.use(async (req, res, next) => {
